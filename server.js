a555e4df04a24a2f601d4e47c4abe81c6e987ed3
jofelgrepoUCSD <fgrepo@ucsd.edu>
2021-06-15 12:48:31 -0700

    Connected to MySL

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 const express = require('express');
 const app = express();
 
+
 // Reference  Database
 const db = require("./models");
 const PORT = process.env.PORT || 3000;
+
+// Bring in Router
+const apiRoutes = require('./routes/apiRoutes');
+app.use("/api",apiRoutes);
 
 //setup stuff
 app.use(express.urlencoded({extended:true}));
